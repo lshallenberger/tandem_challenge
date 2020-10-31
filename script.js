c2616071b41ae1ef99940b2c28f33f011ac0937c
Leighton <leighton.shallenberger@gmail.com>
2020-10-31 02:04:10 -0500

    questions are randomized and have initial function

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
 var fs = require("fs");
 var inquirer = require('inquirer');
-
-function start() {
+function inizio() {
+  inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "ooga booga",
+      choices: ["Practice Trivia", "Add Trivia Questions", "Remove Trivia Questions", "Exit"],
+      name: "username"
+    }
+    
+  ])
+  .then(function(response) {
+    if(response.username === "Practice Trivia") {
+      console.log("1")
+    }
+    else if (response.username === "Add Trivia Questions") {
+      console.log("2")
+    }
+    else if (response.username === "Remove Trivia Questions") {
+      console.log("3")
+    }
+    else {
+      console.log("Buh Bye");
+    }
+  });
+}
+function get_data() {
   fs.readFile("questions.json", "utf8", function(error, data) {
 
     if (error) {
      return console.log(error);
     }
   
-    let banana = JSON.parse(data);
-    // shuffle(banana);
-    let cake = banana[0].incorrect;
-    cake.push(banana[0].correct)
-    shuffle(cake);
-    inquire(banana[0], cake);
-
+    let questions = JSON.parse(data);
+    shuffle(questions);
+    return questions;
   });
 };
-
-function inquire(pine, pudding) {
+let p = 0;
+function trivia(shuffled_questions) {
+  let answers = shuffled_questions[p].incorrect;
+  answers.push(shuffled_questions[p].correct)
+  shuffle(answers);
   inquirer
   .prompt([
     {
       type: "list",
-      message: pine.question,
-      choices: pudding, 
+      message: pine[p].question,
+      choices: answers, 
       name: "username"
     }
     
   ])
   .then(function(response) {
-    if(response.username === pine.correct) {
+    if(response.username === pine[p].correct) {
       console.log("correct");
+      p++;
+      inquire(pine)
     }
     else {
       console.log("wrong");
@@ -41,14 +67,8 @@ function inquire(pine, pudding) {
 
 function shuffle(array) {
   var m = array.length, t, i;
-
-  // While there remain elements to shuffle…
   while (m) {
-
-    // Pick a remaining element…
     i = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
     t = array[m];
     array[m] = array[i];
     array[i] = t;
@@ -57,4 +77,4 @@ function shuffle(array) {
   return array;
 }
 
-start();
+inizio();
