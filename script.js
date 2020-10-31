08e4cbde0aec7aecb8d649356478a3b8e6510a06
Leighton <leighton.shallenberger@gmail.com>
2020-10-31 00:44:59 -0500

    inquire can now tell between correct and incorrect

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
 var fs = require("fs");
 var inquirer = require('inquirer');
 
-function starter() {
+function start() {
   fs.readFile("questions.json", "utf8", function(error, data) {
 
-  if (error) {
-    return console.log(error);
-  }
+    if (error) {
+     return console.log(error);
+    }
   
-  let banana = JSON.parse(data);
-  console.log(banana[0]);
-  // inquire(banana);
-  
+    let banana = JSON.parse(data);
+    // shuffle(banana);
+    let cake = banana[0].incorrect;
+    cake.push(banana[0].correct)
+    shuffle(cake);
+    inquire(banana[0], cake);
+
   });
 };
 
-function inquire(pine) {
+function inquire(pine, pudding) {
   inquirer
   .prompt([
     {
       type: "list",
-      message: pine[0].question,
-      choices:["Trivia Practice", "Add To Question Pool", "Remove From Question Pool"],
+      message: pine.question,
+      choices: pudding, 
       name: "username"
     }
     
   ])
   .then(function(response) {
-
-    console.log(response.username);
+    if(response.username === pine.correct) {
+      console.log("correct");
+    }
+    else {
+      console.log("wrong");
+    }
   });
 };
 
-starter();
+function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+start();
